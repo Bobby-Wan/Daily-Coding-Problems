60fcf911a21c2327690b9ebac06f63f285656a3d
Bozhidar Atanasov <bluesdragon@abv.bg>
2020-09-21 23:12:49 +0300

    DCP2 fix

--- problem2.py
+++ problem2.py
@@ -11,19 +11,38 @@
 import basics
 
 def compute(input_list):
-    output_list = []
-    copy_list = input_list
-    for index, value in enumerate(input_list):
-        result = 1
-        index_number = copy_list[index]
-        copy_list[index] = 1
-        for x in copy_list:
-            result = result * x
-        output_list.append(result)
-        copy_list[index] = index_number
-    return output_list
+    n = len(input_list)
+    products = [1 for i in range(n)]
+    temp = 1
+
+    for i in range(n):
+        products[i] = temp
+        temp *= input_list[i]
+
+    temp = 1
+    for i in range(n-1, -1, -1):
+        products[i] *= temp
+        temp *= input_list[i]
+
+    return products
+
+
+    # output_list = []
+    # copy_list = input_list
+    # for index, value in enumerate(input_list):
+    #     result = 1
+    #     index_number = copy_list[index]
+    #     copy_list[index] = 1
+    #     for x in copy_list:
+    #         result = result * x
+    #     output_list.append(result)
+    #     copy_list[index] = index_number
+    # return output_list
 
 def main():
+    assert compute([3,2,1]) == [2,3,6]
+    assert compute([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24]
+    
     input_list = basics.getNumberList()
     print(compute(input_list))
 
