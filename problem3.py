957476df52750d37727e0fcdd61db1fb2301c7ff
Bozhidar Atanasov <bluesdragon@abv.bg>
2020-09-22 16:42:33 +0300

    DCP 3 fix

--- problem3.py
+++ problem3.py
@@ -23,10 +23,14 @@ class StaticHolder:
     index = 0
 
 def serialize(root):
+    StaticHolder.index = 0
+    return to_string(root)
+
+def to_string(root):
     if root == None:
         result = 'none '
     else:
-        result = root.val + ' ' + serialize(root.left) + serialize(root.right)
+        result = root.val + ' ' + to_string(root.left) + to_string(root.right)
     return result
 
 def deserialize(string):
@@ -35,14 +39,13 @@ def deserialize(string):
     values = string.split()
     if values == []:
         return None
-    print(values[StaticHolder.index])
+    
     if len(values) <= (StaticHolder.index - 1):
         return None
     if values[StaticHolder.index] == 'none':
         StaticHolder.index += 1
         return None
     else:
-        print(StaticHolder.index)
         root = values[StaticHolder.index]
         StaticHolder.index += 1
         left = deserialize(' '.join(values))
@@ -53,9 +56,10 @@ def deserialize(string):
 
 def main():
     node = Node('root', Node('left', Node('left.left')), Node('right'))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
+    assert deserialize(serialize(node)).right.val == 'right'
+
     print(serialize(node))
-    StaticHolder.index = 0
-    print(serialize(deserialize(serialize(node))))
 
 
 if __name__ == '__main__':
