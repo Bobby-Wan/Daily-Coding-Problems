16bbe226aed259a528ec2e20fb13ece4e957bea5
Bozhidar Atanasov <bluesdragon@abv.bg>
2020-09-30 16:13:17 +0300

    DCP 4 fix

--- problem4.py
+++ problem4.py
@@ -5,25 +5,7 @@
 # For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 # You can modify the input array in-place.
 
-import basics
-
 def first_missing(input):
-    #create list consisting of only the positive values
-    # positives = []
-    # for integer in input_list:
-    #     if integer > 0:
-    #         positives.append(integer) 
-
-    # end = len(positives)
-    # for positive in positives:
-    #     if abs(positive) <= end:
-    #         positives[abs(positive)-1] *= -1
-
-    # for i in range(end):
-    #     if positives[i] > 0:
-    #         return i+1
-    
-    # return end + 1
     length = len(input)
     i = 0
     while(i < length):
