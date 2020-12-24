33115f3949dbb8e36892a61ec7cc479124509b9e
Bozhidar <bluesdragon@abv.bg>
2020-12-24 23:50:44 +0200

    added dcp 34 description

--- problem34.py
+++ problem34.py
@@ -1,3 +1,17 @@
+# This problem was asked by Quora.
+# Given a string, find the palindrome that can be made by 
+# inserting the fewest number of characters as possible anywhere 
+# in the word. If there is more than one palindrome of minimum 
+# length that can be made, return the lexicographically earliest 
+# one (the first one alphabetically).
+# For example, given the string "race", you should return "ecarace", 
+# since we can add three letters to it (which is the smallest 
+# amount to make a palindrome). There are seven other palindromes 
+# that can be made from "race" by adding three letters, 
+# but "ecarace" comes first alphabetically.
+# As another example, given the string "google", you should 
+# return "elgoogle".
+
 def is_palidrome(s):
     return s[::-1] == s
 
@@ -9,7 +23,7 @@ def make_palindrome(word):
         return word[0] + make_palindrome(word[1:-1]) + word[-1]
     else:
         pal1 = word[0] + make_palindrome(word[1:]) + word[0]
-        pal2 = word[-1] + make_palindrome(word[:-1]) + word[-1]
+        pal2 = word[-1] + make_palidrome(word[:-1]) + word[-1]
 
         if len(pal1) < len(pal2):
             return pal1
